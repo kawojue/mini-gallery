74534d561756f16c6865d4a2f06072a53a203dd4
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 22:11:38 +0100

    fetched images

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,11 +1,40 @@
+import dataImages from '../data'
 import { createContext, useState, useEffect } from 'react'
 
 const Context = createContext({})
 
 export const DataProvider = ({ children }) => {
+    const [city, setCity] = useState([])
+    const [nature, setNature] = useState([])
+
+    const mapImages = () => {
+        let cities = []
+        let natures = []
+
+        dataImages.forEach(image => {
+            if (image.category === 'city') {
+                cities.push(image)
+            }
+            if (image.category === 'nature') {
+                natures.push(image)
+            }
+        })
+
+        setCity(cities)
+        setNature(natures)
+    }
+
+    useEffect(() => {
+        mapImages()
+    }, [])
+
+
+    console.log(city)
+    console.log(nature)
+
     return (
         <Context.Provider value={{
-
+            city, nature
         }}>
             {children}
         </Context.Provider>
