a54f13de4cc7b6c6ecaca0fb137603035407a933
Raheem Kawojue <kawojue08@gmail.com>
2022-11-16 00:28:41 +0100

    refractor

--- src/data.js
+++ src/data.js
@@ -1,43 +1,43 @@
 const dataImages = [
     {
         id: 1,
-        category: 'city',
-        "image": '../public/images/city-1.jpeg'
+        place: 'city',
+        img: '../images/city-1.jpeg'
     },
     {
         id: 2,
-        category: 'city',
-        "image": '../public/images/city-2.jpeg'
+        place: 'city',
+        img: '../images/city-2.jpeg'
     },
     {
         id: 3,
-        category: 'city',
-        "image": '../public/images/city-3.jpeg'
+        place: 'city',
+        img: '../images/city-3.jpeg'
     },
     {
         id: 4,
-        category: 'city',
-        "image": '../public/images/city-4.jpeg'
+        place: 'city',
+        img: '../images/city-4.jpeg'
     },
     {
         id: 5,
-        category: 'city',
-        "image": '../public/images/city-5.jpeg'
+        place: 'city',
+        img: '../images/city-5.jpeg'
     },
     {
         id: 6,
-        category: 'nature',
-        "image": '../public/images/nature-1.jpeg'
+        place: 'nature',
+        img: '../images/nature-1.jpeg'
     },
     {
         id: 7,
-        category: 'nature',
-        "image": '../public/images/nature-2.jpeg'
+        place: 'nature',
+        img: '../images/nature-2.jpeg'
     },
     {
         id: 8,
-        category: 'nature',
-        "image": '../public/images/nature-3.jpeg'
+        place: 'nature',
+        img: '../images/nature-3.jpeg'
     }
 ]
 
