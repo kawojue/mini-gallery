81c4e8ba93d85a88d2c44f1a056b44e5e9557012
Raheem Kawojue <kawojue08@gmail.com>
2022-11-16 00:48:44 +0100

    responsive

--- src/components/LoadPlace.jsx
+++ src/components/LoadPlace.jsx
@@ -1,8 +1,9 @@
 const LoadPlace = ({ places }) => {
     return (
-        <section className={places}>
+        <section className='location md:grid-cols-2 lg:grid-cols-3'>
             {places.map(place => (
-                <img src={place.img} key={place.id} />
+                <img src={place.img} key={place.id}
+                    className="photo" />
             ))}
         </section>
     )
