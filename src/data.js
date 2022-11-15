a183d2cb3be479b8f82e5a76771fedbf424af769
Raheem Kawojue <kawojue08@gmail.com>
2022-11-15 21:47:47 +0100

    data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const dataImages = [
+    {
+        id: 1,
+        category: 'city',
+        "image": '../public/images/city-1.jpeg'
+    },
+    {
+        id: 2,
+        category: 'city',
+        "image": '../public/images/city-2.jpeg'
+    },
+    {
+        id: 3,
+        category: 'city',
+        "image": '../public/images/city-3.jpeg'
+    },
+    {
+        id: 4,
+        category: 'city',
+        "image": '../public/images/city-4.jpeg'
+    },
+    {
+        id: 5,
+        category: 'city',
+        "image": '../public/images/city-5.jpeg'
+    },
+    {
+        id: 6,
+        category: 'nature',
+        "image": '../public/images/nature-1.jpeg'
+    },
+    {
+        id: 7,
+        category: 'nature',
+        "image": '../public/images/nature-2.jpeg'
+    },
+    {
+        id: 8,
+        category: 'nature',
+        "image": '../public/images/nature-3.jpeg'
+    }
+]
+
+export default dataImages
