88945dffe15d58889fdafb5ccc73e25b7cdf094d
Raheem Kawojue <kawojue08@gmail.com>
2022-11-17 12:08:09 +0100

    view

--- src/components/LoadPlace.jsx
+++ src/components/LoadPlace.jsx
@@ -1,9 +1,14 @@
+import Context from './Context'
+import { useContext } from 'react'
+
+
 const LoadPlace = ({ places }) => {
+    const { view } = useContext(Context)
     return (
         <article className='location md:grid-cols-2 lg:grid-cols-3'>
             {places.map(place => (
-                <img src={place.img} key={place.id}
-                    className="photo" />
+                <img src={place.img} key={place.id} className="photo"
+                    onClick={() => view(place.id, place.place, place)} />
             ))}
         </article>
     )
