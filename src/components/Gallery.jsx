2e6537b8e7918910e77c152a07f0eaca5294748f
Raheem Kawojue <kawojue08@gmail.com>
2022-11-18 13:30:20 +0100

    refractor

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,29 +1,31 @@
 import Context from './Context'
 import { useContext } from 'react'
 import { MdClose } from 'react-icons/md'
+import GalleryPreview from './GalleryPreview'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
+
 
 const Gallery = () => {
 
-    const { openModal, setOpenModal } = useContext(Context)
+    const { move, activeImage, close } = useContext(Context)
 
     return (
         <>
             <button className="close"
-                onClick={() => setOpenModal(!openModal)}>
+                onClick={() => close()}>
                 <MdClose />
             </button>
             <div>
-                <article className="show-conatiner">
-                    <FaAngleLeft className="ico" />
-                    <img src="../../images/city-1.jpeg" className="w-96 image" />
-                    <FaAngleRight className="ico" />
+                <article className="show-img-conatiner">
+                    <button className="ico" onClick={() => move('prev')}>
+                        <FaAngleLeft />
+                    </button>
+                    <img src={activeImage.img} className="w-96 image" />
+                    <button className="ico" onClick={() => move('prev')} >
+                        <FaAngleRight />
+                    </button>
                 </article>
-                <article className="preview-container">
-                    <img src="../../images/city-1.jpeg" className="w-28 img" />
-                    <img src="../../images/city-2.jpeg" className="w-28 img" />
-                    <img src="../../images/city-3.jpeg" className="w-28 img" />
-                </article>
+                <GalleryPreview />
             </div>
         </>
     )
