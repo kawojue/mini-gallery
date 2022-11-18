43873252eaa0989bd4c74146bec848f178a4dccc
Raheem Kawojue <kawojue08@gmail.com>
2022-11-19 00:06:00 +0100

    fixed img switch & display

--- src/components/GalleryPreview.jsx
+++ src/components/GalleryPreview.jsx
@@ -1,15 +1,18 @@
 import Context from './Context'
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 
-const GalleryPreview = () => {
-    const { } = useContext(Context)
+const GalleryPreview = ({ data }) => {
+    const { setActiveImage, setActive, activeList, active } = useContext(Context)
+    const [activeImg, setActiveImg] = useState(false)
+
+    const handlePreview = (img) => {
+        setActiveImage(img)
+        setActive(activeList.indexOf(img))
+    }
 
     return (
-        <article className="preview-container">
-            <img src="../../images/city-1.jpeg" className="w-28 img" />
-            <img src="../../images/city-2.jpeg" className="w-28 img" />
-            <img src="../../images/city-3.jpeg" className="w-28 img" />
-        </article>
+        <img src={data.img} className={`img ${activeImg ? 'active' : ''}`}
+            alt={data.title} onClick={() => handlePreview(data)} />
     )
 }
 
