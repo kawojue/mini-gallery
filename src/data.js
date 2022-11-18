bbeae75e2b62c3ad5a3f6cab313f68f5fbefc103
Raheem Kawojue <kawojue08@gmail.com>
2022-11-18 14:02:32 +0100

    added title

--- src/data.js
+++ src/data.js
@@ -2,41 +2,49 @@ const dataImages = [
     {
         id: 1,
         place: 'city',
+        title: 'city-1',
         img: '../images/city-1.jpeg'
     },
     {
         id: 2,
         place: 'city',
+        title: 'city-2',
         img: '../images/city-2.jpeg'
     },
     {
         id: 3,
         place: 'city',
+        title: 'city-3',
         img: '../images/city-3.jpeg'
     },
     {
         id: 4,
         place: 'city',
+        title: 'city-4',
         img: '../images/city-4.jpeg'
     },
     {
         id: 5,
         place: 'city',
+        title: 'city-5',
         img: '../images/city-5.jpeg'
     },
     {
         id: 6,
         place: 'nature',
+        title: 'nature-1',
         img: '../images/nature-1.jpeg'
     },
     {
         id: 7,
         place: 'nature',
+        title: 'nature-2',
         img: '../images/nature-2.jpeg'
     },
     {
         id: 8,
         place: 'nature',
+        title: 'nature-3',
         img: '../images/nature-3.jpeg'
     }
 ]
