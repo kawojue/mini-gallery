5a75afda342d1b81320420b92813550b23d19b58
Raheem Kawojue <kawojue08@gmail.com>
2022-11-22 13:08:14 +0100

    edit data

--- src/data.js
+++ src/data.js
@@ -1,52 +1,48 @@
-const dataImages = [
-    {
-        city: [
-            {
-                id: 1,
-                title: 'city-1',
-                img: '../images/city-1.jpeg',
-            },
-            {
-                id: 2,
-                title: 'city-2',
-                img: '../images/city-2.jpeg',
-            },
-            {
-                id: 3,
-                title: 'city-3',
-                img: '../images/city-3.jpeg',
-            },
-            {
-                id: 4,
-                title: 'city-4',
-                img: '../images/city-4.jpeg',
-            },
-            {
-                id: 5,
-                title: 'city-5',
-                img: '../images/city-5.jpeg',
-            },
-        ]
-    },
-    {
-        nature: [
-            {
-                id: 1,
-                title: 'nature-1',
-                img: '../images/nature-1.jpeg',
-            },
-            {
-                id: 2,
-                title: 'nature-2',
-                img: '../images/nature-2.jpeg',
-            },
-            {
-                id: 3,
-                title: 'nature-3',
-                img: '../images/nature-3.jpeg',
-            }
-        ]
-    }
+const data = [
+    [
+        {
+            id: 1,
+            title: 'nature-1',
+            img: '../images/nature-1.jpeg',
+        },
+        {
+            id: 2,
+            title: 'nature-2',
+            img: '../images/nature-2.jpeg',
+        },
+        {
+            id: 3,
+            title: 'nature-3',
+            img: '../images/nature-3.jpeg',
+        }
+    ],
+    [
+        {
+            id: 1,
+            title: 'city-1',
+            img: '../images/city-1.jpeg',
+        },
+        {
+            id: 2,
+            title: 'city-2',
+            img: '../images/city-2.jpeg',
+        },
+        {
+            id: 3,
+            title: 'city-3',
+            img: '../images/city-3.jpeg',
+        },
+        {
+            id: 4,
+            title: 'city-4',
+            img: '../images/city-4.jpeg',
+        },
+        {
+            id: 5,
+            title: 'city-5',
+            img: '../images/city-5.jpeg',
+        },
+    ]
 ]
 
-export default dataImages
+export default data
